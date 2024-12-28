fd99af19140d0c02405fb46b2abc840de0730c40
NickolayBogdanov <Nick9315357565@gmail.com>
2024-12-29 01:59:36 +0300

    Add files via upload

--- add_input.py
+++ add_input.py
@@ -1,5 +1,7 @@
 username = input('введите ваше имя: ')
-title = input('введите название заметки: ')
+subtitle1 = input('введите заголовок: ')
+subtitle2 = input('введите заголовок 2: ')
+subtitle3 = input('введите заголовок 3: ')
 content = input('введите текст заметки: ')
 status = input('введите статус заметки: ')
 

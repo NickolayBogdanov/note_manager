da82cc4952e16b2abbd701029b5bcb435ef5ee76
NickolayBogdanov <Nick9315357565@gmail.com>
2024-12-29 01:57:29 +0300

    Add files via upload

---
+++ final.py
@@ -0,0 +1,14 @@
+from note_manager.add_input import username, content, status
+from note_manager.add_list import title
+from note_manager.data_changer import created_date, issue_date
+
+
+note = [
+    username,
+    title,
+    content,
+    status,
+    created_date,
+    issue_date
+]
+print(note)

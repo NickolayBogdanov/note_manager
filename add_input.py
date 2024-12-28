f8ec5a7b05fbf3c71adffaeb8926aa42226cf066
NickolayBogdanov <Nick9315357565@gmail.com>
2024-12-28 23:38:13 +0300

    Add files via upload

---
+++ add_input.py
@@ -0,0 +1,7 @@
+username = input('введите ваше имя: ')
+title = input('введите название заметки: ')
+content = input('введите текст заметки: ')
+status = input('введите статус заметки: ')
+
+from note_manager.data_changer import created_date
+from note_manager.data_changer import issue_date

abf0f94f37e9373114147d375fabff7dd5785b54
NickolayBogdanov <Nick9315357565@gmail.com>
2025-01-02 10:47:04 +0300

    Add files via upload

--- add_list.py
+++ add_list.py
@@ -1,3 +1,4 @@
 from note_manager.add_input import title
 
+# title = [subtitle1, subtitle2, subtitle3] изменено за словарь "content" для удобства работы и отображения
 titles = [title]
--- final.py
+++ final.py
@@ -1,4 +1,4 @@
-from note_manager.add_input import username, status, title, subtitle1, subtitle2, subtitle3, subcontent1, subcontent2, subcontent3
+from note_manager.add_input import username, title, status, subtitle1, subtitle2, subtitle3, subcontent1, subcontent2, subcontent3
 from note_manager.add_list import titles
 from note_manager.data_changer import temp_created_date, temp_issue_date
 # импорт данных из других фаилов программы

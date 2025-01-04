f8e2ff2722beabdf0afde048ff0339908e8de8ae
NickolayBogdanov <Nick9315357565@gmail.com>
2025-01-04 17:21:08 +0300

    Add files via upload

--- final.py
+++ final.py
@@ -1,9 +1,13 @@
-from note_manager.add_input import username, content, status, title
+from note_manager.add_input import username, content, status
 from note_manager.add_list import titles
-from note_manager.data_changer import created_date, issue_date
+from note_manager.data_changer import temp_created_date, temp_issue_date
 
-note = {
-    username: titles,
-    title: (content, status, created_date, issue_date)
-     } # словарь по имени предоставляющий список заголовков а по заголовку предоставляющий саму заметку и данные о заметке
+note = [
+    username,
+    titles,
+    content,
+    status,
+    temp_created_date,
+    temp_issue_date
+]
 print(note)

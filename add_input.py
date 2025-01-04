da8d39738d36ad63de96a7021293a016fdb3b823
NickolayBogdanov <Nick9315357565@gmail.com>
2025-01-04 19:11:01 +0300

    Update add_input.py

--- add_input.py
+++ add_input.py
@@ -1,8 +1,5 @@
-from note_manager.data_changer import created_date, issue_date
-
 username = input('введите ваше имя: ')
 title = input('введите заголовок: ')
 content = input('введите заметку: ')
 status = input('введите статус заметки: ')
-
-
+from note_manager.data_changer import created_date, issue_date

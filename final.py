78fc51af7a25dbb1890a510f5315636514658fe3
NickolayBogdanov <Nick9315357565@gmail.com>
2025-01-05 20:59:12 +0300

    Add files via upload

--- final.py
+++ final.py
@@ -1,13 +1,17 @@
-from note_manager.add_input import username, content, status
+from note_manager.add_input import username, title, content, status
 from note_manager.add_list import titles
 from note_manager.data_changer import temp_created_date, temp_issue_date
 
 note = [
     username,
-    titles,
-    content,
-    status,
-    temp_created_date,
-    temp_issue_date
+    {title: (
+        content,
+        status,
+        temp_created_date,
+        temp_issue_date
+        )
+    },
+    titles
 ]
+
 print(note)

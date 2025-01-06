329dc83074280951d41785040bd7efb83d78b9e5
NickolayBogdanov <Nick9315357565@gmail.com>
2025-01-06 15:11:53 +0300

    Add files via upload

--- update_status.py
+++ update_status.py
@@ -1,15 +1,16 @@
-status_base = {"1": "Выполнено", "2": "В процессе", "3": "Отложено"}
-status = status_base.get("2")
+status_base = {1: "Выполнено", 2: "В процессе", 3: "Отложено"}
+status = status_base.get(2)
 
 print("текущий статус заметки: ", status)
 print("выберите статус заметки: 1 - выполнено, 2 - в процессе, 3 - отложено")
 
 while True:
-    status = status_base.get(input("Выберите статус заметки \"1\", \"2\" или \"3\": "))
-    if  status != status_base.get("1") and status != status_base.get("2") and status != status_base.get("3"):
-        status_base = {"1": "Выполнено", "2": "В процессе", "3": "Отложено"}
+    status = status_base.get(int(input("Выберите статус заметки \"1\", \"2\" или \"3\": ")))
+    if  status != status_base.get(1) and status != status_base.get(2) and status != status_base.get(3):
+        status_base = {1: "Выполнено", 2: "В процессе", 3: "Отложено"}
         print("неверно введено значение")
-    else:5
+    else:
+        print("Стаус успешно изменен, текущий статус: ", (status))
         break
 
-print("Стаус успешно изменен, текущий статус: ", (status))
+

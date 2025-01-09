8b985ef4c97dc28d5a20ca569a9e5ac1f2df8afb
NickolayBogdanov <Nick9315357565@gmail.com>
2025-01-09 13:03:55 +0300

    Add files via upload

--- data_changer.py
+++ data_changer.py
@@ -1,7 +1,15 @@
 import datetime
-created_date = datetime.date.today() # текущая дата
+
+
+created_date = datetime.datetime.today() # текущая дата
 print(f"Дата создания заметки в формате \"дд-мм-гггг\":{created_date.strftime("%d-%m-%Y")}") # выводим дату в нужный формат
-issue_date = input("Дата истечения заметки в формате \"дд-мм-гггг\":") # получаем от пользователя дату
-issue_date = (datetime.datetime.strptime(issue_date, "%d-%m-%Y")) # конвертируем дату в объект
-temp_created_date = (created_date.strftime("%d-%m")) # выводимая пользователю информация в удобном формате
-temp_issue_date = (issue_date.strftime("%d-%m")) # выводимая пользователю информация в удобном формате
+while True:
+    try:
+        issue_date = input("Введите дату истечения заметки в формате \"дд-мм-гггг\": ")# получаем от пользователя дату
+        issue_date = (datetime.datetime.strptime(issue_date, "%d-%m-%Y"))  # конвертируем дату в объект
+        break
+    except (ValueError):
+        print("Неверный формат, попробуйте снова.")
+temp_created_date = f"дата создания заметки: {created_date.strftime("%d-%m")}" # выводимая пользователю информация в удобном формате
+temp_issue_date = f"дата создания заметки: {issue_date.strftime("%d-%m")}" # выводимая пользователю информация в удобном формате
+

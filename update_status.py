04ceb26d02984f3974d9f4addcea5a2faeb43d0d
NickolayBogdanov <Nick9315357565@gmail.com>
2025-01-10 23:35:42 +0300

    Add files via upload

--- update_status.py
+++ update_status.py
@@ -1,16 +1,16 @@
-status_base = {1: "Выполнено", 2: "В процессе", 3: "Отложено"}
+status_base = {1: "Выполнено", 2: "В процессе", 3: "Отложено"} # предусмотренные статусы
 status = status_base.get(2)
-
 print("текущий статус заметки: ", status)
 print("выберите статус заметки: 1 - выполнено, 2 - в процессе, 3 - отложено")
 
 while True:
-    status = status_base.get(int(input("Выберите статус заметки \"1\", \"2\" или \"3\": ")))
-    if  status != status_base.get(1) and status != status_base.get(2) and status != status_base.get(3):
-        status_base = {1: "Выполнено", 2: "В процессе", 3: "Отложено"}
-        print("неверно введено значение")
-    else:
-        print("Стаус успешно изменен, текущий статус: ", (status))
-        break
-
-
+    try:
+        status = status_base.get(int(input("Выберите статус заметки \"1\", \"2\" или \"3\": ")))
+        if  status != status_base.get(1) and status != status_base.get(2) and status != status_base.get(3):
+            status_base = {1: "Выполнено", 2: "В процессе", 3: "Отложено"}
+            print("неверно введено значение")
+        else:
+            print("Стаус успешно изменен, текущий статус: ", (status))
+            break
+    except ValueError:
+        print("Неверный формат, попробуйте снова.")

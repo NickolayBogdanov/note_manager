ab2b7e7df5044b40caa6c70b6c967593363fc2a7
NickolayBogdanov <Nick9315357565@gmail.com>
2025-01-14 12:49:26 +0300

    Add files via upload

---
+++ delete_note.py
@@ -0,0 +1,61 @@
+
+
+notes = [
+    {
+        "username": "Павел",
+        "title": "Дела на завтра",
+        "content": "Помыть посуду, сделать уроки, в бассейн к 18:30",
+        "status": "В процессе",
+        "created_date": '11-01-25',
+        "issue_date": '12-01-25',
+    },
+    {
+        "username": "Николай",
+        "title": "Учеба",
+        "content": "Взять книги в библиотеке",
+        "status": "В процессе",
+        "created_date": '11-01-25',
+        "issue_date": '20-01-25',
+    }
+
+]
+
+def delete():
+    print("Список заметок:")
+    print(notes)
+    note_del = 0
+    delete = input("Введите имя или заголовок заметки которую желаете удалить: ") # переменная для сравнения
+    while True:
+        for i in range(len(notes)): # цикл переборщик для поиска и сравнения
+            for k in notes[i].values():
+                if str.upper(k) == str.upper(delete): # действия в случае совпадения
+                    ''' при удалении найденой заметки сразу, если она не в конце списка нарушается цикл и вылетает ошибка,
+                    чтобы этого избежать переносим найденую заметку в конец списка'''
+                    note_del = notes[i]
+                    notes.append(note_del)
+                    notes.pop(i)
+
+        if note_del == 0:
+            print("заметка не найдена.")
+            break
+        else:
+            yes_no = input(f'Уверены, что хотите удалить: {note_del["title"]}  \"да\" \"нет\" : ')
+            try:
+                if yes_no == "нет":
+                    print("До новых встреч.")
+                    break
+                elif yes_no == "да":
+                    notes.pop(-1)
+                    print("Заметка удалена.")
+                    break
+                else:
+                    print("Неверно введено значение, попробуйте снова.")
+            except ValueError:
+                print("Неверный формат, попробуйте снова.")
+
+    print(notes)
+
+
+delete()
+
+

6e463a47027c6b19b957e038946dca83bf36ffd3
NickolayBogdanov <Nick9315357565@gmail.com>
2025-01-17 21:13:21 +0300

    Add files via upload

--- add_titles_loop.py
+++ add_titles_loop.py
@@ -1,9 +1,9 @@
 titles = []
 
 while True:
-    titles.append(input(str("введите заголовок (чтобы остановить введите стоп или не вводите ничего): ")))
-    print(titles[-1])
-    if titles[-1] == str("стоп") or titles[-1] == str("") or titles[-1] == str(" "):
+    titles.append(input("введите заголовок (чтобы остановить введите стоп или не вводите ничего): "))
+    title = titles[-1]
+    if titles[-1] == ("стоп") or titles[-1] == (""):
         titles.pop(-1)
         break
 print(titles)

883cc5de4ab1af4e92d8fd7f07057b6ba50f5b95
NickolayBogdanov <Nick9315357565@gmail.com>
2025-01-21 21:25:45 +0300

    Add files via upload

--- display_notes_function.py
+++ display_notes_function.py
@@ -3,13 +3,14 @@ from colorama import Fore, init
 
 
 
+
 notes = [
     {
         "username": "Павел",
         "title": "Дела на завтра",
         "content": "Помыть посуду, сделать уроки, в бассейн к 18:30",
         "status": "В процессе",
-        "created_date": '11-01-25',
+        "created_date": '12-01-25',
         "issue_date": '15-01-25',
     },
     {
@@ -25,7 +26,7 @@ notes = [
         "title": "Учеба",
         "content": "Взять книги в библиотеке",
         "status": "В процессе",
-        "created_date": '11-01-25',
+        "created_date": '10-01-25',
         "issue_date": '20-01-25',
     }
 ]
@@ -34,42 +35,37 @@ notes = [
 
 
 def display_all_notes(note_number = 1):
-    sorted_list = sorted(notes, key=itemgetter("issue_date"))
     init(autoreset=True)
-    for i in range(len(sorted_list)):
-        print(Fore.RED + "---------------------------------")
-        print(Fore.YELLOW + f"Note №{note_number}")
-        note_number = note_number + 1
-        string_namber = 1
-        for k in sorted_list[i].items():
-            print(Fore.GREEN + f"{string_namber})", *k)
-            string_namber = string_namber + 1
-    if sorted_list == []:
-        print(Fore.RED + "You don't have any saved notes.")
+    sorted_list_1 = sorted(notes, key=itemgetter("created_date"))
+    sorted_list_2 = sorted(notes, key=itemgetter("issue_date"))
+    sorted_option = {"1": "created_date", "2": "issue_date"}
+    print(sorted_option)
+    option = input("Enter a note sorting option \"1\" or \"2\": ")
+    if option == "1":
+        for i in range(len(sorted_list_1)):
+            print(Fore.RED + "----------------------------------------------------")
+            print(Fore.YELLOW + f"Note №{note_number}")
+            note_number = note_number + 1
+            string_namber = 1
+            for k in (sorted_list_1[i].items()):
+                name, val = k
+                print(Fore.GREEN + f"{string_namber})", "{:<20} {:<15}".format(name, val))
+                string_namber = string_namber + 1
+        if sorted_list_1 == []:
+            print(Fore.RED + "You don't have any saved notes.")
+    elif option == "2":
+        for i in range(len(sorted_list_2)):
+            print(Fore.RED + "----------------------------------------------------")
+            print(Fore.YELLOW + f"Note №{note_number}")
+            note_number = note_number + 1
+            string_namber = 1
+            for k in sorted_list_2[i].items():
+                name, val = k
+                print(Fore.GREEN + f"{string_namber})", "{:<20} {:<15}".format(name, val))
+                string_namber = string_namber + 1
+        if sorted_list_2 == []:
+            print(Fore.RED + "You don't have any saved notes.")
 
 display_all_notes()
 
 
-def display_signs_notes(note_number = 1):
-    init(autoreset=True)
-    notes_based_on = []
-    based  = input(("Enter the search object (recommended name or status): "))
-    for i in range(len(notes)):
-        for k in notes[i].values():
-            if str.upper(k) == str.upper(based):
-                notes_based_on.append(notes[i])
-
-    sorted_list = sorted(notes_based_on, key=itemgetter("issue_date"))
-    for i in range(len(sorted_list)):
-        print(Fore.RED + "---------------------------------")
-        print(Fore.YELLOW + f"Note №{note_number}")
-        note_number = note_number + 1
-        string_namber = 1
-
-        for k in sorted_list[i].items():
-            print(Fore.GREEN + f"{string_namber})", *k)
-            string_namber = string_namber + 1
-    if sorted_list == []:
-        print(Fore.RED + "You don't have any saved notes.")
-
-display_signs_notes()

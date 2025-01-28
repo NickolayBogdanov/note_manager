054250e235342e3a5ec8e53b5791aed872ea3090
NickolayBogdanov <Nick9315357565@gmail.com>
2025-01-28 23:29:21 +0300

    Add files via upload

---
+++ Этап4_Обработка_Ошибок_Богданов-Катьков_Николай.py
@@ -0,0 +1,36 @@
+import yaml
+from colorama import Fore, init
+
+def load_notes_from_file(file_name = input("Введите название файла: ")):
+    init(autoreset=True)
+    try:
+        with open(file_name, encoding='utf-8') as file:
+            while True:
+                notes = yaml.load(file, Loader=yaml.FullLoader)
+                if notes == None:
+                    print("Фаил пуст.")
+                    break
+                else:
+                    note_number = 1
+                    for i in range(len(notes)):
+                        print(Fore.RED + "----------------------------------------------------")
+                        # разделительная линния (красная)
+                        print(Fore.YELLOW + f"Note №{note_number}")  # нумерация заметок (желтая)
+                        note_number = note_number + 1
+                        string_namber = 1
+                        for k in notes[i].items():
+                            name, val = k
+                            print(Fore.GREEN + f"{string_namber})", "{:<15} {:<30}".format(name, val))
+                            # нумерация строк (зеленая) и габариты таблицы
+                            string_namber = string_namber + 1
+                    break
+        file.close()
+    except FileNotFoundError:
+        print(Fore.RED + "Фаил не найден.")
+        file = open(file_name, "w", encoding='utf-8')
+        file.close()
+        print(f"Новый файл {file}, успешно создан.")
+    except Exception as fails:
+        print(Fore.RED + f"Ошибка: {fails}")
+
+load_notes_from_file()
